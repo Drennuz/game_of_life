bf8fb7fa3ee4825e5d03fa6c9e2c66f51203c9a1
Drennuz <joysun10@gmail.com>
2012-11-08 23:49:08 +0800

    20121108

--- gol.ml
+++ gol.ml
@@ -1,4 +1,11 @@
-type board = {width:int; height: int; state: int array array};;
-type cell = {life: int; neighbors: int};;
 
+(* type declaration *)
+type cell = {life: int; neighbors: int};;
+type board = {width:int; height: int; state: cell array array};;
+
+(* define constants *)
+let width = 905;;
+let height_life = 605;;
+let life_size = 15;;
+let edge = 5;;
 
