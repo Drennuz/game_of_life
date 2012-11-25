4e0ad757e3cfdb50c875618d49a19e4a812896f1
Drennuz <joysun10@gmail.com>
2012-11-25 23:25:28 +0800

    bug fix; boundary cells

--- gol.ml
+++ gol.ml
@@ -4,7 +4,8 @@ type board = {width:int; height: int; state: cell array array};;
 
 (* define constants *)
 let total_height = 750;;
-let width_life = 905;;
+let total_width = 905;;
+let width_life = total_width;;
 let height_life = 565;;
 let height_stat_box = total_height - height_life;;
 let life_size = 10;;
@@ -57,31 +58,42 @@ let update_board_graph bd =
     done
 
 let initiate_graph inputPercent = 
-    Graphics.open_graph (" " ^ (string_of_int width_life) ^ "x" ^ (string_of_int (total_height)));
+    Graphics.open_graph (" " ^ (string_of_int total_width) ^ "x" ^ (string_of_int (total_height)));
     let seed = int_of_float (inputPercent *. (float_of_int hCount) *. (float_of_int wCount)) in
     let bod = initiate_abs seed in
     update_board_graph bod;
     bod;;
 
-let update_mid_cell bd i j = 
-    let count = ref 0 in
-    for k = (j - 1) to (j + 1) do if bd.state.(i-1).(k).life = 1 then count:=!count + 1 done;
-    for k = (j - 1) to (j + 1) do if bd.state.(i+1).(k).life = 1 then count:=!count + 1 done;
-    if bd.state.(i).(j-1).life = 1 then count:=!count + 1;
-    if bd.state.(i).(j+1).life = 1 then count:=!count + 1;
-    let newCell = {bd.state.(i).(j) with neighbors = !count} in
-    if bd.state.(i).(j).life = 0 then
-        (if !count = 3 then {newCell with life = 1} else newCell)
-    else
-        (if (!count < 2 || !count > 3) then {newCell with life = 0} else newCell);;
-        
+exception InvalidValue;;
 
+let update_cell bd i j = 
+    let i_low = if i < 1 then (bd.height - 1) else (i - 1) 
+    and i_high = if (i + 1) > (bd.height - 1) then 0 else (i+1)
+    and j_low = if j < 1 then (bd.width - 1) else (j-1)
+    and j_high = if (j+1) > (bd.width - 1) then 0 else (j+1) in
+    let count = ref 0 in
+    if bd.state.(i_low).(j_low).life = 1 then count:=!count + 1;
+    if bd.state.(i_low).(j).life = 1 then count:=!count + 1;
+    if bd.state.(i_low).(j_high).life = 1 then count:=!count + 1;
+    if bd.state.(i).(j_low).life = 1 then count:=!count + 1;
+    if bd.state.(i).(j_high).life = 1 then count:=!count + 1;
+    if bd.state.(i_high).(j_low).life = 1 then count:=!count + 1;
+    if bd.state.(i_high).(j).life = 1 then count:=!count + 1;
+    if bd.state.(i_high).(j_high).life = 1 then count:=!count + 1;
+    
+    let newCell = {bd.state.(i).(j) with neighbors = !count} in
+    match bd.state.(i).(j).life with
+        0 -> (if !count == 3 then {newCell with life = 1; changed = true} else {newCell with changed = false})
+        |1 -> (if ((!count < 2) || (!count > 3)) then {newCell with life = 0; changed = true} else {newCell with changed = false})
+        |_ -> raise InvalidValue;;
+        
 let update_board bd = 
-    for i = 1 to (bd.height - 2) do
-        for j = 1 to (bd.width - 2) do
-            bd.state.(i).(j) <- update_mid_cell bd i j
+    for i = 0 to (bd.height - 1) do
+        for j = 0 to (bd.width - 1) do
+            bd.state.(i).(j) <- update_cell bd i j
         done;
     done;;
+    
 
 let go () = 
     if (Array.length Sys.argv) != 3 then print_string "usage: gol count seed\n" else    
@@ -91,7 +103,7 @@ let go () =
         minisleep sleepTime; update_board bod; update_board_graph bod 
     done);;
 
-(* testing area *)
+(* main program *)
 
 generate_seed ();;
 go ();;
